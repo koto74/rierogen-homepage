253a881d9a2e9fb6decdbf9c22c6866d2f609a1a
koto74 <kotoganaiyo@gmail.com>
2023-03-19 08:38:02 +0900

    add hoge

--- pages/index.js
+++ pages/index.js
@@ -18,13 +18,9 @@ const Home = () => {
     const model = treeModel
     return (
         <Box h="100%">
-            <Canvas camera={{ position: [-10, 10, 40], fov: 50 }}>
-                <ambientLight intensity={0.75} color="whlte" groundColor="blue" />
-                <spotLight position={[50, 50, 10]} angle={0.15} penumbra={1} />
-                <Model position={[0, 0.25, 0]} url={model} />
-                <ContactShadows scale={20} blur={10} far={20} />
-                <OrbitControls />
-            </Canvas>
+            <Heading>
+                hoge
+            </Heading>
         </Box>
     )
 }
